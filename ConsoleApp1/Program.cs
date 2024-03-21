997ee4a62dcc3b384e9fee782537db3dc4defa0f
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2024-03-21 18:50:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string apiKey = "AIzaSyDjfTO_wi4dwzmq1yHVVGwJW6ady9kV-LU";
+string videoId = "krsBRQbOPQ4"; // Идентификатор видео, например "krsBRQbOPQ4"
+
+string apiUrl = $"https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id={videoId}&key={apiKey}";
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        HttpResponseMessage response = await client.GetAsync(apiUrl);
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+        dynamic videoData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+
+        if (videoData.items.Count > 0)
+        {
+            string title = videoData.items[0].snippet.title;
+            string channelTitle = videoData.items[0].snippet.channelTitle;
+            int viewCount = videoData.items[0].statistics.viewCount;
+
+            Console.WriteLine($"Информация о видео:");
+            Console.WriteLine($"Название: {title}");
+            Console.WriteLine($"Канал: {channelTitle}");
+            Console.WriteLine($"Количество просмотров: {viewCount}");
+        }
+        else
+        {
+            Console.WriteLine("Видео не найдено.");
+        }
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine($"Ошибка при отправке запроса: {e.Message}");
+    }
+}
