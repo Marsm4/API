2e7298b43a5672bac3efb22049a00cd03056295b
212213 <marselmubaraksin231@mail.ru>
2024-03-23 09:18:28 +0300

    asf

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,74 +1,185 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
-string apiKey = "AIzaSyDjfTO_wi4dwzmq1yHVVGwJW6ady9kV-LU";
-string videoId = "MSJpsY9Smig"; // адрес видео
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
-string videoApiUrl = $"https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id={videoId}&key={apiKey}";
-
-using (HttpClient client = new HttpClient())
+namespace YourNamespace
 {
-    try
+    class Program
     {
-        HttpResponseMessage videoResponse = await client.GetAsync(videoApiUrl);
-        videoResponse.EnsureSuccessStatusCode();
+        static ITelegramBotClient _botClient;
 
-        string videoResponseBody = await videoResponse.Content.ReadAsStringAsync();
-        dynamic videoData = Newtonsoft.Json.JsonConvert.DeserializeObject(videoResponseBody);
+        static ReceiverOptions _receiverOptions;
 
-        if (videoData.items.Count > 0)
+        async Task Main(string[] args)
         {
-            string title = videoData.items[0].snippet.title;
-            string channelTitle = videoData.items[0].snippet.channelTitle;
-            int viewCount = videoData.items[0].statistics.viewCount;
-            int likeCount = videoData.items[0].statistics.likeCount;
-            string channelId = videoData.items[0].snippet.channelId;
+            string apiKey = "a17bccc339c9b0489cfa485dd5d5c387";
+            string videoId = "dQw4w9WgXcQ"; //адрес видео
 
-            Console.WriteLine($"Информация о видео:");
-            Console.WriteLine($"Название: {title}");
-            Console.WriteLine($"Канал: {channelTitle}");
-            Console.WriteLine($"Количество просмотров: {viewCount}");
-            Console.WriteLine($"Количество лайков: {likeCount}");
+            string apiUrl = $"https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id={videoId}&key={apiKey}";
 
-            // Запрос информации о канале
-            string channelApiUrl = $"https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id={channelId}&key={apiKey}";
-            HttpResponseMessage channelResponse = await client.GetAsync(channelApiUrl);
-            channelResponse.EnsureSuccessStatusCode();
-
-            string channelResponseBody = await channelResponse.Content.ReadAsStringAsync();
-            dynamic channelData = Newtonsoft.Json.JsonConvert.DeserializeObject(channelResponseBody);
-
-            if (channelData.items.Count > 0)
+            _botClient = new TelegramBotClient("YOUR_TELEGRAM_BOT_TOKEN");
+            _receiverOptions = new ReceiverOptions
             {
-                string channelName = channelData.items[0].snippet.title;
-                string channelUsername = channelData.items[0].snippet.customUrl; // Никнейм, если указан
-                string channelCreationDate = channelData.items[0].snippet.publishedAt;
-                int subscriberCount = channelData.items[0].statistics.subscriberCount;
-                int videoCount = channelData.items[0].statistics.videoCount;
-
-                Console.WriteLine();
-                Console.WriteLine($"Информация о канале:");
-                Console.WriteLine($"Название канала: {channelName}");
-                if (!string.IsNullOrEmpty(channelUsername))
+                AllowedUpdates = new[]
                 {
-                    Console.WriteLine($"Никнейм канала: {channelUsername}");
-                }
-                Console.WriteLine($"Количество подписчиков: {subscriberCount}");
-                Console.WriteLine($"Дата создания канала: {channelCreationDate}");
-                Console.WriteLine($"Количество видео на канале: {videoCount}");
-            }
-            else
-            {
-                Console.WriteLine("Информация о канале не найдена.");
-            }
+                    UpdateType.Message,
+                },
+
+                ThrowPendingUpdates = true,
+            };
+
+            using var cts = new CancellationTokenSource();
+
+            _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token);
+
+            var me = await _botClient.GetMeAsync();
+            Console.WriteLine($"{me.FirstName} запущен!");
+
+            await Task.Delay(-1);
         }
-        else
+
+        static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            Console.WriteLine("Видео не найдено.");
+            // Обязательно ставим блок try-catch, чтобы наш бот не "падал" в случае каких-либо ошибок
+            try
+            {
+                // Сразу же ставим конструкцию switch, чтобы обрабатывать приходящие Update
+                switch (update.Type)
+                {
+                    case UpdateType.Message:
+                        {
+                            var message = update.Message;
+                            var user = message.From;
+                            var chat = message.Chat;
+
+                            Console.WriteLine($"{user.FirstName} ({user.Id}) написал сообщение: {message.Text}");
+
+                            switch (message.Text)
+                            {
+                                case "/start":
+                                    {
+                                        var replyKeyboard = new ReplyKeyboardMarkup(
+                                            new List<KeyboardButton[]>()
+                                            {
+                                                new KeyboardButton[]
+                                                {
+                                                    new KeyboardButton("пака"),
+                                                    new KeyboardButton("привет"),
+                                                }
+                                            })
+                                        {
+                                            ResizeKeyboard = true,
+                                        };
+                                        await botClient.SendTextMessageAsync(
+                                            chat.Id,
+                                            "Выбери клавиатуру:\n" +
+                                            "/inline\n" +
+                                            "/reply\n",
+                                            replyMarkup: replyKeyboard);
+
+                                        break;
+                                    }
+                                case "/reply":
+                                    {
+                                        var replyKeyboard = new ReplyKeyboardMarkup(
+                                            new List<KeyboardButton[]>()
+                                            {
+                                                new KeyboardButton[]
+                                                {
+                                                    new KeyboardButton("пака"),
+                                                    new KeyboardButton("привет"),
+                                                }
+                                            })
+                                        {
+                                            ResizeKeyboard = true,
+                                        };
+                                        await botClient.SendTextMessageAsync(
+                                            chat.Id,
+                                            "Это reply клавиатура!",
+                                            replyMarkup: replyKeyboard);
+
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        await botClient.SendTextMessageAsync(
+                                            chat.Id,
+                                            "Используй только текст!");
+                                        break;
+                                    }
+                            }
+
+                            break;
+                        }
+                }
+
+                // После обработки сообщения выводим информацию о видео
+                string apiKey = "a17bccc339c9b0489cfa485dd5d5c387";
+                string videoId = "dQw4w9WgXcQ"; // адрес видео
+
+                string apiUrl = $"https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id={videoId}&key={apiKey}";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+                        response.EnsureSuccessStatusCode();
+
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        dynamic videoData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+
+                        if (videoData.items.Count > 0)
+                        {
+                            string title = videoData.items[0].snippet.title;
+                            string channelTitle = videoData.items[0].snippet.channelTitle;
+                            int viewCount = videoData.items[0].statistics.viewCount;
+                            int likeCount = videoData.items[0].statistics.likeCount;
+
+                            Console.WriteLine($"Информация о видео:");
+                            Console.WriteLine($"Название: {title}");
+                            Console.WriteLine($"Канал: {channelTitle}");
+                            Console.WriteLine($"Количество просмотров: {viewCount}");
+                            Console.WriteLine($"Количество лайков: {likeCount}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Видео не найдено.");
+                        }
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine($"Ошибка при отправке запроса: {e.Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
-    }
-    catch (HttpRequestException e)
-    {
-        Console.WriteLine($"Ошибка при отправке запроса: {e.Message}");
+
+        static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
+        {
+            // Тут создадим переменную, в которую поместим код ошибки и её сообщение 
+            var ErrorMessage = error switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => error.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
     }
 }
